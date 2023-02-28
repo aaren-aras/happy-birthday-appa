e02d1db860ae64e14b3313ffd97979e206b39b66
Aaren Arasaratnam <aaren.aras@gmail.com>
2023-02-27 20:22:32 -0500

    add basic setup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # happy-birthday-appa
+
+Credits: https://www.youtube.com/watch?v=HeT-5RZgEQY 
---
+++ script.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById('canvas1');
+// Short form for context 
+const ctx = canvas.getContext('2d');
+
+const image1 = new Image();
+image1.src = ' '; 
